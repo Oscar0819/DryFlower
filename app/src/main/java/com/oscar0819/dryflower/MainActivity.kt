141878edabcf1f7f91722836019966b7b35399f8
Oscar0819 <dn08193@gmail.com>
2025-07-21 10:23:54 +0900

    core:feature 모듈 추가

--- app/src/main/java/com/oscar0819/dryflower/MainActivity.kt
+++ app/src/main/java/com/oscar0819/dryflower/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.oscar0819.dryflower
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.oscar0819.dryflower.ui.theme.DryFlowerTheme
@@ -34,10 +41,17 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    var text by remember { mutableStateOf("") }
+
+    Column {
+        TextField(
+            value = text,
+            onValueChange =  { newText ->
+                text = newText
+                Log.d("test", newText)
+            }
+        )
+    }
 }
 
 @Preview(showBackground = true)

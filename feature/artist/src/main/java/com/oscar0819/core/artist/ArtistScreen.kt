7c2002f2520d9005d18c527071bcc3d5e6a69564
Oscar0819 <dn08193@gmail.com>
2025-08-22 18:03:36 +0900

    [ArtistScreen] - 인기곡 디테일한 디자인 수정

--- feature/artist/src/main/java/com/oscar0819/core/artist/ArtistScreen.kt
+++ feature/artist/src/main/java/com/oscar0819/core/artist/ArtistScreen.kt
@@ -4,21 +4,25 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -42,7 +46,6 @@ import coil.request.ImageRequest
 import com.oscar0819.core.preview.PreviewUtils
 import com.oscar0819.designsystem.component.DryFlowerAppBar
 import com.oscar0819.designsystem.component.DryFlowerCircularProgress
-import kotlin.math.max
 import kotlin.math.min
 
 @Composable
@@ -78,23 +81,27 @@ fun ArtistScreen(
         ) {
             when (uiState) {
                 is ArtistUiState.Success -> {
-                    val songVisibilityCnt = 4
-                    val maxPageCnt = 4
-                    val pageCnt = getPageCount(
-                        uiState.artistSongInfos.size,
-                        maxPageCnt,
-                        songVisibilityCnt,
+                    val pageCnt = calculatePageCount(
+                        totalItemSize = uiState.artistSongInfos.size,
+                        maxPageCount = 4,
+                        itemPerPage = 4,
                     )
 
-                    val topSongsPagerState = rememberPagerState(pageCount = { pageCnt })
+                    val topSongsPagerState = rememberPagerState(
+                        pageCount = {
+                            pageCnt
+                        }
+                    )
 
                     LazyColumn(
                         modifier = Modifier
+                            .padding(start = 8.dp)
                             .fillMaxSize(),
                         verticalArrangement = Arrangement.spacedBy(16.dp),
                     ) {
                         artistName(uiState)
                         topSongs(uiState, topSongsPagerState)
+                        album(uiState)
                     }
                 }
                 ArtistUiState.Loading -> {
@@ -131,16 +138,15 @@ private fun LazyListScope.topSongs(
 ) {
     item {
         // TODO 행 갯수로 표현할 수 없는 음악은 화살표로 전체 볼 수 있게 하기
-        Text(
-            text = stringResource(R.string.top_songs),
-            fontSize = 24.sp,
-            fontWeight = FontWeight.Bold,
-        )
-
-        HorizontalPager(topSongsPagerState) { page ->
+        ArtistCategoryText(R.string.top_songs)
+        Spacer(modifier = Modifier.height(12.dp))
+        HorizontalPager(
+            topSongsPagerState,
+            contentPadding = PaddingValues(horizontal = 8.dp)
+        ) { page ->
             Column {
                 repeat(4) { i ->
-                    val index = page * 4 + i + 1 // 첫번째
+                    val index = page * 4 + i + 1 // 0번째는 더미 데이터라 1부터 시작
 
                     if (index >= uiState.artistSongInfos.size) return@repeat
 
@@ -153,29 +159,57 @@ private fun LazyListScope.topSongs(
 
                     val imageRequest = builder.build()
 
-                    Row(modifier = Modifier.fillParentMaxWidth()) {
-                        AsyncImage(
-                            model = imageRequest,
-                            contentDescription = "앨범 이미지",
-                            contentScale = ContentScale.Fit,
+                    val imageSize = 54
+
+                    Row(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .fillParentMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Card(
                             modifier = Modifier
-                                .size(54.dp)
-                        )
+                                .size(imageSize.dp)
+                            ,
+                            shape = RoundedCornerShape(6.dp),
+                            elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+                        ) {
+                            AsyncImage(
+                                model = imageRequest,
+                                contentDescription = "앨범 이미지",
+                                contentScale = ContentScale.Fit,
+                                modifier = Modifier
+                                    .fillMaxSize()
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(14.dp))
 
                         Column {
                             Text(
                                 text = "${artistSongsInfos[index].trackName}",
+                                fontSize = 16.sp,
                                 color = MaterialTheme.colorScheme.onSurface,
                                 maxLines = 1,
+                                modifier = Modifier.padding(end = 32.dp)
                             )
-                            Spacer(Modifier.width(8.dp))
+                            Spacer(Modifier.height(2.dp))
                             Text(
                                 text = "${artistSongsInfos[index].collectionName}",
+                                fontSize = 12.sp,
                                 color = Color.Gray,
                                 maxLines = 1,
                                 overflow = TextOverflow.Ellipsis,
+                                modifier = Modifier.padding(end = 32.dp)
                             )
                         }
+                    }
+                    if (i != 3) {
+                        HorizontalDivider(
+                            Modifier.padding(start = (imageSize + 6).dp, end = 32.dp),
+                            DividerDefaults.Thickness,
+                            DividerDefaults.color
+                        )
                     }
                 }
             }
@@ -183,23 +217,40 @@ private fun LazyListScope.topSongs(
     }
 }
 
-private fun getPageCount(
-    artistSongInfoListSize: Int,
+private fun LazyListScope.album(
+    uiState: ArtistUiState.Success,
+) {
+    item {
+        ArtistCategoryText(R.string.album)
+
+    }
+}
+
+@Composable
+private fun ArtistCategoryText(id: Int) {
+    Text(
+        text = stringResource(id),
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+    )
+}
+
+private fun calculatePageCount(
+    totalItemSize: Int,
     maxPageCount: Int,
-    maxItemVisibilityCnt: Int,
+    itemPerPage: Int,
 ): Int {
-    val size = artistSongInfoListSize - 1
+    val size = totalItemSize - 1
 
-    val existRemainder = if ((size % maxItemVisibilityCnt) != 0) true else false
+    val existRemainder = if ((size % itemPerPage) != 0) true else false
 
-    val pagesNeeded = size / maxItemVisibilityCnt
+    val pagesNeeded = size / itemPerPage
 
     val pageCnt = if (existRemainder) {
         min(pagesNeeded + 1, maxPageCount)
     } else {
         min(pagesNeeded, maxPageCount)
     }
-    // 기본적으로 1페이지를 필요로 함.
     return pageCnt
 }
 
